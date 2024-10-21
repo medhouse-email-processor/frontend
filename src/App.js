74ef617d3bd969ee48c87c230bc18b48e54cdbe7
Alikhan Baidussenov <a.baydusenov@mail.ru>
2024-10-21 12:40:36 +0000

    Removed all routes except one

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import FetchFiles from './pages/FetchFiles'
-import UploadFiles from './pages/UploadFiles'
 import FetchAndUpload from './pages/FetchAndUpload'
 import './App.css'
 
@@ -12,14 +10,10 @@ function App() {
         <nav>
           <ul>
             <li><a href="/">Fetch Files</a></li>
-            <li><a href="/upload">Upload Files</a></li>
-            <li><a href="/fetchupload">Fetch and Upload</a></li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<FetchFiles />} />
-          <Route path="/upload" element={<UploadFiles />} />
-          <Route path="/fetchupload" element={<FetchAndUpload />} />
+          <Route path="/" element={<FetchAndUpload />} />
         </Routes>
       </div>
     </Router>
